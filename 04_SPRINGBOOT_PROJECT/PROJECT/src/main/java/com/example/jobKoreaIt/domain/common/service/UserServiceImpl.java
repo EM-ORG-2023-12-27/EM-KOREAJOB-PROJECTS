21379bf40a72a17f8e9d163ce3587e7d24453949
jungwoogyun <jwg8910@naver.com>
2024-06-03 12:41:38 +0900

    .

--- 04_SPRINGBOOT_PROJECT/PROJECT/src/main/java/com/example/jobKoreaIt/domain/common/service/UserServiceImpl.java
+++ 04_SPRINGBOOT_PROJECT/PROJECT/src/main/java/com/example/jobKoreaIt/domain/common/service/UserServiceImpl.java
@@ -2,6 +2,8 @@ package com.example.jobKoreaIt.domain.common.service;
 
 
 import com.example.jobKoreaIt.domain.common.repository.UserRepository;
+import com.example.jobKoreaIt.domain.offer.repository.JobOfferRepository;
+import com.example.jobKoreaIt.domain.seeker.repository.JobSeekerRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,6 +14,10 @@ public class UserServiceImpl implements UserService {
 
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private JobSeekerRepository jobSeekerRepository;
+    @Autowired
+    private JobOfferRepository jobOfferRepository;
 
 
 }
