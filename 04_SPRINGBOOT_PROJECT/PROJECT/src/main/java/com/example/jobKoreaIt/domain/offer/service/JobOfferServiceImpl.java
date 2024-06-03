f65808abb098a65f2b7fbfaca543d18cc154b837
jungwoogyun <jwg8910@naver.com>
2024-06-03 12:20:09 +0900

    .

--- 04_SPRINGBOOT_PROJECT/PROJECT/src/main/java/com/example/jobKoreaIt/domain/offer/service/JobOfferServiceImpl.java
+++ 04_SPRINGBOOT_PROJECT/PROJECT/src/main/java/com/example/jobKoreaIt/domain/offer/service/JobOfferServiceImpl.java
@@ -1,6 +1,8 @@
 package com.example.jobKoreaIt.domain.offer.service;
 
 
+import com.example.jobKoreaIt.domain.offer.entity.Company;
+import com.example.jobKoreaIt.domain.offer.repository.CompanyRepository;
 import com.example.jobKoreaIt.domain.offer.repository.JobOfferRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,5 +19,32 @@ public class JobOfferServiceImpl {
     @Transactional(rollbackFor = Exception.class)
     public void function(){
         log.info("TEST...");
+    }
+
+    //-----------------------
+    //박정현 Company CRUD
+    //-----------------------
+    @Autowired
+    private CompanyRepository companyRepository;
+
+    @Transactional(rollbackFor = Exception.class)
+    public void CompanyRegistration(){
+        log.info("회사등록...");
+    }
+    @Transactional(rollbackFor = Exception.class)
+    public void ModifyCompany(){
+        log.info("회사정보수정...");
+    }
+    @Transactional(rollbackFor = Exception.class)
+    public void RemoveCompany(){
+        log.info("회사정보 삭제...");
+    }
+    @Transactional(rollbackFor = Exception.class)
+    public void AddCompany(){
+        log.info("회사정보 추가...");
+    }
+    @Transactional(rollbackFor = Exception.class)
+    public void ShowCompany(){
+        log.info("회사정보 확인...");
     }
 }
