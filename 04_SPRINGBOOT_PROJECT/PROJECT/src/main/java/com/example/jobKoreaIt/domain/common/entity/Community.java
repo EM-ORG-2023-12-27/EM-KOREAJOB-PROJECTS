463fbf0d922f40de81c2d38259d16eaa6f719835
piepenie <piepenie@daum.net>
2024-06-24 16:25:34 +0900

    .

--- 04_SPRINGBOOT_PROJECT/PROJECT/src/main/java/com/example/jobKoreaIt/domain/common/entity/Community.java
+++ 04_SPRINGBOOT_PROJECT/PROJECT/src/main/java/com/example/jobKoreaIt/domain/common/entity/Community.java
@@ -1,11 +1,5 @@
 package com.example.jobKoreaIt.domain.common.entity;
 
-
-<<<<<<< HEAD
-import com.example.jobKoreaIt.domain.common.dto.CommunityDto;
-import jakarta.persistence.*;
-import lombok.*;
-=======
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -19,39 +13,12 @@ import org.springframework.format.annotation.DateTimeFormat;
 import org.yaml.snakeyaml.tokens.ScalarToken;
 
 import java.time.LocalDateTime;
->>>>>>> dev
 
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-<<<<<<< HEAD
-@Getter
-@Setter
-@Table(name = "Community_table")
-public class Community {
-    @Id // pk 컬럼 지정. 반드시 필수.
-    @GeneratedValue(strategy = GenerationType.IDENTITY) // Mysql 기준으로 Auto_increment
-    private Long id;
 
-    @Column(length = 8, nullable = false) // length 컬럼 크기, not null
-    private String cat;
-
-    @Column // default 값 255, null 가능.
-    private String title;
-
-    @Column
-    private String username;
-
-    @Column(length = 500)
-    private String content;
-
-//    public static Community toSaveEntity(CommunityDto communityDto){
-//
-//    }
-    // 빌더 패턴을 사용햇자나! 그럼 빌더로 써야지.
-
-=======
 @Data
 public class Community {
     @Id
@@ -64,6 +31,5 @@ public class Community {
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime regdate;
     private Long count;
->>>>>>> dev
 
 }
